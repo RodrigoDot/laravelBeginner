6ef6252405710ec772ee6540346d533c7bc52750
RodrigoDot <rodrigodot@outlook.com>
2017-09-16 19:16:06 -0300

    atualizacao README

--- README.md
+++ README.md
@@ -3,8 +3,22 @@
 ## Requirements
 
 1. MySQL server
-2. Composer with global install
 
 ## How to use
 
-1. Go to ``laravelBeginner-master/laravel/ 
+#### The database
+
+1. Go to ``laravelBeginner-master/laravel/.env``
+2. Change the setting to your database
+
+#### Migrating the tables
+
+1. Using the terminal or console, go to ``laravelBeginner-master/laravel``
+2. Run ``php artisan migrate``
+3. Run ``php artisan serve``
+4. In your browser put the address of your server
+
+#### Test seeders
+
+1. Using the terminal or console, go to ``laravelBeginner-master/laravel``
+2. Run ``php artisan db:seed``
--- banco.sql
+++ banco.sql
@@ -4,7 +4,17 @@ create database laravel1;
 
 use laravel1;
 
+show tables;
+
 select * from pages;
 select * from users;
 
 
+update pages
+set user_id = 2 
+where id = 4; 
+
+
+1	Dr. Deven King	stefan.kozey@example.org	$2y$10$1heSN91AJUeZuMaIEKWOCerRNpydNxBXEZUQPBqmJ2L0wD2zhi.ZG	ucXAWBaGAd	2017-09-16 14:09:52	2017-09-16 14:09:52
+2	Cornelius Monahan	angus.pouros@example.net	$2y$10$1heSN91AJUeZuMaIEKWOCerRNpydNxBXEZUQPBqmJ2L0wD2zhi.ZG	KQIhoZHPf1	2017-09-16 14:09:52	2017-09-16 14:09:52
+3	Angelita Blanda	lucius.reilly@example.net	$2y$10$1heSN91AJUeZuMaIEKWOCerRNpydNxBXEZUQPBqmJ2L0wD2zhi.ZG	6tEZgNRnVB	2017-09-16 14:09:52	2017-09-16 14:09:52
